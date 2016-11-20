e57202c3b0aa943dbdf2d55d64b72353502141a9
nao215 <n.yanagisawa215@gmail.com>
2016-11-21 00:20:43 +0900

    Isomorphic

--- learnyoureact/program.js
+++ learnyoureact/program.js
@@ -1,26 +1,63 @@
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var DOM = React.DOM;
+var body = DOM.body;
+var div = DOM.div;
+var script = DOM.script;
+
+var browserify = require('browserify');
+var babelify = require("babelify");
+
 var express = require('express');
 var app = express();
 
 app.set('port', (process.argv[2] || 3000));
 app.set('view engine', 'jsx');
 app.set('views', __dirname + '/views');
-app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
+app.engine('jsx', require('express-react-views').createEngine({transformViews: false}));
 
 require('babel/register')({
     ignore: false
 });
 
-// ↓に記述
-var data = [{
-  title: 'Shopping',
-  detail: process.argv[3]
-},{
-  title: 'Hair cut',
-  detail: process.argv[4]
-}];
+var TodoBox = require('./views/index.jsx');
 
-app.use('/', function(req, res) {
-  res.render('index', {data: data});
+var data = [
+    {title: 'Shopping', detail: process.argv[3]},
+    {title: 'Hair cut', detail: process.argv[4]}
+];
+
+app.use('/bundle.js', function (req, res) {
+    res.setHeader('content-type', 'application/javascript');
+
+    browserify({debug: true})
+        .transform(babelify.configure({
+            presets: ["react", "es2015"],
+            compact: false
+        }))
+        .require("isomorphic/solution/app.js", {entry: true})
+        .bundle()
+        .pipe(res);
 });
 
-app.listen(app.get('port'), function() {});
+app.use('/', function (req, res) {
+    var initialData = JSON.stringify(data);
+    var markup = ReactDOMServer.renderToString(React.createElement(TodoBox, {data: data}));
+
+    res.setHeader('Content-Type', 'text/html');
+
+    var html = ReactDOMServer.renderToStaticMarkup(body(null,
+        div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}),
+        script({
+            id: 'initial-data',
+            type: 'text/plain',
+            'data-json': initialData
+        }),
+        script({src: '/bundle.js'})
+    ));
+
+    res.end(html);
+});
+
+app.listen(app.get('port'), function () {
+});
